500f6f52fbe23d44bba3de76c5c06df592367fd2
Gabe <gabriel.tyler2003@gmail.com>
2022-04-10 18:52:50 -0400

    Start Execute

--- Execute/mymachine.cpp
+++ Execute/mymachine.cpp
@@ -16,6 +16,8 @@ using i32 = std::int_fast32_t;
 using u64 = std::uint_fast64_t;
 using i64 = std::int_fast64_t;
 
+// have alu be a separate class?
+
 namespace Opcodes
 {
     enum Categories 
@@ -34,6 +36,16 @@ namespace Opcodes
         { UNIMPL, UNIMPL, UNIMPL, UNIMPL, UNIMPL, UNIMPL, UNIMPL, UNIMPL },
         // Fourth row (inst[6:5] = 0b11)
         { BRANCH, JALR, UNIMPL, JAL, SYSTEM, UNIMPL, UNIMPL, UNIMPL }
+    };
+}
+
+namespace Alu
+{
+    enum Commands 
+    {
+        ALU_ADD, ALU_SUB, ALU_MUL, ALU_DIV,
+        ALU_REM, ALU_SLL, ALU_SRL, ALU_SRA,
+        ALU_AND, ALU_OR,  ALU_XOR, ALU_NOT
     };
 }
 
@@ -64,19 +76,36 @@ public:
         // same as in FetchOut: cout << do << '\n';
         friend std::ostream& operator<<(std::ostream& out, const Machine::DecodeOut& dec);
     };
+    struct ExecuteOut 
+    {
+        i64 result;
+        u8  n, z, c, v;
+
+        friend std::ostream& operator<<(std::ostream& out, const ExecuteOut& eo) 
+        {
+            std::ostringstream sout;
+            sout << "Result: " << eo.result << " [NZCV]: " 
+                << (u32)eo.n 
+                << (u32)eo.z 
+                << (u32)eo.c
+                << (u32)eo.v;
+            return out << sout.str();
+        }
+    };
 
 public:
     static constexpr i32 NUM_REGS = 32;
     static constexpr i32 MEM_SIZE = 1 << 18;
 
 private:
-    char* _memory;           // The memory.
-    i32 _memorySize;         // The size of the memory (should be MEM_SIZE)
+    char* _memory;       // The memory.
+    i32 _memorySize;     // The size of the memory (should be MEM_SIZE)
     i64 _pc;             // The program counter
     i64 _regs[NUM_REGS]; // The register file
 
-    FetchOut  _fo; // Result of the fetch() method.
-    DecodeOut _do; // Result of the decode() method
+    FetchOut _FO; // Result of the fetch() method
+    DecodeOut _DO; // Result of the decode() method
+    ExecuteOut _EO; // Result of the execute() method
 
 public:
     Machine(char* mem, i32 size);
@@ -89,8 +118,11 @@ public:
 
     void Fetch();
     void Decode();
+    void Execute();
+
     FetchOut& DebugFetchOut();
     DecodeOut& DebugDecodeOut();
+    ExecuteOut& DebugExecuteOut();
 
 private:
     // Read from the internal memory
@@ -115,9 +147,11 @@ private:
     void DecodeB();
     void DecodeU();
     void DecodeJ();
+
+    ExecuteOut ALU(Alu::Commands cmd, i64 left, i64 right);
 };
 
-std::ostream& operator<<(std::ostream& out, const Machine::FetchOut&  fo) 
+std::ostream& operator<<(std::ostream& out, const Machine::FetchOut& fo) 
 {
     std::ostringstream sout;
     sout << "0x" << std::hex << std::setfill('0') << std::right << std::setw(8) << fo.instruction;
@@ -133,44 +167,33 @@ std::ostream& operator<<(std::ostream& out, const Machine::DecodeOut& dec)
     switch (dec.op) 
     {
     case LUI:
-        sout << "LUI";
-        break;
+        sout << "LUI"; break;
     case AUIPC:
-        sout << "AUIPC";
-        break;
+        sout << "AUIPC"; break;
     case LOAD:
-        sout << "LOAD";
-        break;
+        sout << "LOAD"; break;
     case STORE:
-        sout << "STORE";
-        break;
+        sout << "STORE"; break;
     case OP_IMM:
-        sout << "OPIMM";
-        break;
+        sout << "OPIMM"; break;
     case OP_IMM_32:
-        sout << "OPIMM32";
-        break;
+        sout << "OPIMM32"; break;
     case OP:
-        sout << "OP";
-        break;
+        sout << "OP"; break;
     case OP_32:
-        sout << "OP32";
-        break;
+        sout << "OP32"; break;
     case BRANCH:
-        sout << "BRANCH";
-        break;
+        sout << "BRANCH"; break;
     case JALR:
-        sout << "JALR";
-        break;
+        sout << "JALR"; break;
     case JAL:
-        sout << "JAL";
-        break;
+        sout << "JAL"; break;
     case SYSTEM:
-        sout << "SYSTEM";
-        break;
+        sout << "SYSTEM"; break;
     case UNIMPL:
-        sout << "NOT-IMPLEMENTED";
-        break;
+        sout << "NOT-IMPLEMENTED"; break;
+    default: 
+        sout << "???";
     }
     sout << '\n';
     sout << "RD       : " << (u32)dec.rd << '\n';
@@ -179,6 +202,16 @@ std::ostream& operator<<(std::ostream& out, const Machine::DecodeOut& dec)
     sout << "offset   : " << dec.offset << '\n';
     sout << "left     : " << dec.left_val << '\n';
     sout << "right    : " << dec.right_val;
+    return out << sout.str();
+}
+std::ostream& operator<<(std::ostream& out, const Machine::ExecuteOut& eo) 
+{
+    std::ostringstream sout;
+    sout << "Result: " << eo.result << " [NZCV]: " 
+        << (u32)eo.n 
+        << (u32)eo.z 
+        << (u32)eo.c
+        << (u32)eo.v;
     return out << sout.str();
 }
 
@@ -214,24 +247,24 @@ void Machine::SetXReg(i32 which, i64 value)
 void Machine::Fetch()
 {
     // read the instruction at the program counter memory address
-    _fo.instruction = MemoryRead<u32>(_pc);
+    _FO.instruction = MemoryRead<u32>(_pc);
 }
 void Machine::Decode() 
 {
     using namespace Opcodes;
 
-    u8 OpcodeMapRow = (_fo.instruction >> 5) & 0b11;
-    u8 OpcodeMapCol = (_fo.instruction >> 2) & 0b111;
-    u8 InstSize     =  _fo.instruction & 0b11;
+    u8 OpcodeMapRow = (_FO.instruction >> 5) & 0b11;
+    u8 OpcodeMapCol = (_FO.instruction >> 2) & 0b111;
+    u8 InstSize     =  _FO.instruction & 0b11;
     if (InstSize != 3) 
     {
         std::cerr << "[DECODE] Invalid instruction (not a 32-bit instruction).\n";
         return;
     }
 
-    _do.op = OC_MAP[OpcodeMapRow][OpcodeMapCol];
-    // Decode the rest of _do based on the instruction type
-    switch (_do.op)
+    _DO.op = OC_MAP[OpcodeMapRow][OpcodeMapCol];
+    // Decode the rest of _DO based on the instruction type
+    switch (_DO.op)
     {
     case LOAD:
     case JALR:
@@ -258,17 +291,89 @@ void Machine::Decode()
         DecodeR();
         break;
     default:
-        std::cerr << "Invalid op type: " << _do.op << '\n';
+        std::cerr << "Invalid op type: " << _DO.op << '\n';
         break;
     }
 }
-Machine::FetchOut&  Machine::DebugFetchOut()
+void Machine::Execute() 
 {
-    return _fo;
+    using namespace Opcodes;
+    using namespace Alu;
+
+    Commands cmd;
+
+    // Most instructions will follow left/right
+    // but some won't, so we need these:
+    i64 op_left  = _DO.left_val;
+    i64 op_right = _DO.right_val; 
+    
+    if (_DO.op == BRANCH) 
+    {
+        // A branch needs to subtract the operands
+        cmd = ALU_SUB;
+    }
+    else if (_DO.op == LOAD || _DO.op == STORE) 
+    {
+        // For loads and stores, we need to add the
+        // offset with the base register.
+        cmd = ALU_ADD;
+    }
+    else if (_DO.op == OP) 
+    {
+        // We can't tell which ALU command to use until
+        // we read the funct3 and funct7
+        switch (_DO.funct3) 
+        {
+        case 0b000: // ADD or SUB
+            if (_DO.funct7 == 0) 
+            {
+                cmd = ALU_ADD;
+            }
+            else if (_DO.funct7 == 32) 
+            {
+                cmd = ALU_SUB;
+            }
+            break;
+        // Finish the rest of the OP functions here.
+        }
+    }
+    else if (_DO.op == OP_32) 
+    {
+        op_left  = SignExtend(op_left,  31);
+        op_right = SignExtend(op_right, 31);
+        // You still need to determine the ALU command
+    }
+    else if (_DO.op == OP_IMM) 
+    {
+        // Look and see which ALU op needs to be executed.
+    }
+    else if (_DO.op == OP_IMM_32) 
+    { 
+        op_left  = SignExtend(op_left,  31);
+        op_right = SignExtend(op_right, 31);
+        // This is just like OP_IMM except we truncated
+        // the left and right ops.
+    }
+    else if (_DO.op == JALR) 
+    {
+        // JALR has an offset and a register value that
+        // need to be added together.
+        cmd = ALU_ADD;
+    }
+    _EO = ALU(cmd, op_left, op_right);
+}
+
+Machine::FetchOut& Machine::DebugFetchOut()
+{
+    return _FO;
 }
 Machine::DecodeOut& Machine::DebugDecodeOut()
 {
-    return _do;
+    return _DO;
+}
+Machine::ExecuteOut& Machine::DebugExecuteOut()
+{
+    return _EO;
 }
 
 template <typename T>
@@ -299,64 +404,107 @@ i64 Machine::SignExtend(u64 value, u32 index)
 
 void Machine::DecodeR()
 {
-    _do.rd     = (_fo.instruction >> 7)  & 0x1f;
-    _do.funct3 = (_fo.instruction >> 12) & 0b111;
-    _do.funct7 = (_fo.instruction >> 25) & 0x3f;
-    _do.offset = 0ll;
-    _do.left_val  = GetXReg(_fo.instruction >> 15);
-    _do.right_val = GetXReg(_fo.instruction >> 20);
+    _DO.rd     = (_FO.instruction >> 7)  & 0x1f;
+    _DO.funct3 = (_FO.instruction >> 12) & 0b111;
+    _DO.funct7 = (_FO.instruction >> 25) & 0x3f;
+    _DO.offset = 0ll;
+    _DO.left_val  = GetXReg(_FO.instruction >> 15);
+    _DO.right_val = GetXReg(_FO.instruction >> 20);
 }
 void Machine::DecodeI()
 {
-    _do.rd     = (_fo.instruction >> 7)  & 0x1f;
-    _do.funct3 = (_fo.instruction >> 12) & 0b111;
-    _do.funct7 = 0;
-    _do.offset = 0ll;
-    _do.left_val  = GetXReg(_fo.instruction >> 15);
-    _do.right_val = SignExtend((_fo.instruction >> 20), 11u);
+    _DO.rd     = (_FO.instruction >> 7)  & 0x1f;
+    _DO.funct3 = (_FO.instruction >> 12) & 0b111;
+    _DO.funct7 = 0;
+    _DO.offset = 0ll;
+    _DO.left_val  = GetXReg(_FO.instruction >> 15);
+    _DO.right_val = SignExtend((_FO.instruction >> 20), 11u);
 }
 void Machine::DecodeS()
 {
-    _do.rd     = 0;
-    _do.funct3 = (_fo.instruction >> 12) & 0b111;
-    _do.funct7 = 0;
-    _do.offset = SignExtend(((_fo.instruction >> 7)  & 0x1f) |
-                           (((_fo.instruction >> 25) & 0x7f) << 5), 11u);
-    _do.left_val  = GetXReg(_fo.instruction >> 15);
-    _do.right_val = GetXReg(_fo.instruction >> 20);
+    _DO.rd     = 0;
+    _DO.funct3 = (_FO.instruction >> 12) & 0b111;
+    _DO.funct7 = 0;
+    _DO.offset = SignExtend(((_FO.instruction >> 7)  & 0x1f) |
+                           (((_FO.instruction >> 25) & 0x7f) << 5), 11u);
+    _DO.left_val  = GetXReg(_FO.instruction >> 15);
+    _DO.right_val = GetXReg(_FO.instruction >> 20);
 }
 void Machine::DecodeB()
 {
-    _do.rd     = 0;
-    _do.funct3 = (_fo.instruction >> 12) & 0b111;
-    _do.funct7 = 0;
-    _do.offset = SignExtend((((_fo.instruction >> 31) & 1) << 12)   |
-                            (((_fo.instruction >> 25) & 0x3f) << 5) |
-                            (((_fo.instruction >> 8)  & 0xf) << 1)  | 
-                            (((_fo.instruction >> 7)  & 1) << 11), 11u);
-    _do.left_val  = GetXReg(_fo.instruction >> 15); // get_xreg truncates for us
-    _do.right_val = GetXReg(_fo.instruction >> 20);
+    _DO.rd     = 0;
+    _DO.funct3 = (_FO.instruction >> 12) & 0b111;
+    _DO.funct7 = 0;
+    _DO.offset = SignExtend((((_FO.instruction >> 31) & 1) << 12)   |
+                            (((_FO.instruction >> 25) & 0x3f) << 5) |
+                            (((_FO.instruction >> 8)  & 0xf) << 1)  | 
+                            (((_FO.instruction >> 7)  & 1) << 11), 11u);
+    _DO.left_val  = GetXReg(_FO.instruction >> 15); // get_xreg truncates for us
+    _DO.right_val = GetXReg(_FO.instruction >> 20);
 }
 void Machine::DecodeU()
 {
-    _do.rd     = (_fo.instruction >> 7) & 0x1f;
-    _do.funct3 = 0;
-    _do.funct7 = 0;
-    _do.offset = SignExtend((((_fo.instruction >> 12) & 0xf'ffff) << 12), 31u);
-    _do.left_val  = 0ll;
-    _do.right_val = 0ll;
+    _DO.rd     = (_FO.instruction >> 7) & 0x1f;
+    _DO.funct3 = 0;
+    _DO.funct7 = 0;
+    _DO.offset = SignExtend((((_FO.instruction >> 12) & 0xf'ffff) << 12), 31u);
+    _DO.left_val  = 0ll;
+    _DO.right_val = 0ll;
 }
 void Machine::DecodeJ()
 {
-    _do.rd     = (_fo.instruction >> 7) & 0x1f;
-    _do.funct3 = 0;
-    _do.funct7 = 0;
-    _do.offset = SignExtend((((_fo.instruction >> 31) & 1) << 19) | 
-                             ((_fo.instruction >> 21) & 0x3ff)    |
-                            (((_fo.instruction >> 20) & 1) << 10) |
-                            (((_fo.instruction >> 12) & 0xff) << 11), 19u);
-    _do.left_val  = 0ll;
-    _do.right_val = 0ll;
+    _DO.rd     = (_FO.instruction >> 7) & 0x1f;
+    _DO.funct3 = 0;
+    _DO.funct7 = 0;
+    _DO.offset = SignExtend((((_FO.instruction >> 31) & 1) << 19) | 
+                             ((_FO.instruction >> 21) & 0x3ff)    |
+                            (((_FO.instruction >> 20) & 1) << 10) |
+                            (((_FO.instruction >> 12) & 0xff) << 11), 19u);
+    _DO.left_val  = 0ll;
+    _DO.right_val = 0ll;
+}
+
+Machine::ExecuteOut Machine::ALU(Alu::Commands cmd, i64 left, i64 right)
+{
+    using namespace Alu;
+
+    ExecuteOut ret;
+
+    switch (cmd) 
+    {
+    case ALU_ADD:
+        ret.result = left + right;
+        break;
+    case ALU_SUB:
+        ret.result = left - right;
+        break;
+    case ALU_MUL:
+        ret.result = left * right;
+        break;
+    case ALU_DIV:
+        ret.result = left / right;
+        break;
+    case ALU_REM:
+        ret.result = left % right;
+        break;
+    case ALU_SRL:
+        ret.result = static_cast<u64>(left) >> right;
+        break;
+        // Finish the commands here.
+    }
+
+    // Now that we have the result, determine the flags.
+    u8 sign_left   = (left  >> 63) & 1;
+    u8 sign_right  = (right >> 63) & 1;
+    u8 sign_result = (ret.result >> 63) & 1;
+
+    ret.z = !ret.result;
+    ret.n = sign_result;
+    ret.v = ~sign_left & ~sign_right & sign_result |
+             sign_left & sign_right & ~sign_result;
+    ret.c = (ret.result > left) || (ret.result > right);
+
+    return ret;
 }
 
 int main(int argc, char* argv[])
@@ -411,6 +559,8 @@ int main(int argc, char* argv[])
         std::cout << coolMachine.DebugFetchOut() << '\n';
         coolMachine.Decode();
         std::cout << coolMachine.DebugDecodeOut() << '\n';
+        coolMachine.Execute();
+        std::cout << coolMachine.DebugExecuteOut() << '\n';
         coolMachine.SetPC(coolMachine.GetPC() + 4);
     }
 
